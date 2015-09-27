1e4c8eb7077fe5fc121c9bcbb71834718fab75d2
unknown <saravananpushparaj@gmail.com>
2015-09-27 15:59:03 +0530

    V1.0

---
+++ src/test/java/Day2/githublearning.java
@@ -0,0 +1,22 @@
+package Day2;
+
+import org.testng.annotations.Test;
+
+
+	public class githublearning {
+		
+		@Test(groups={"SmokeTest"})
+		
+		public void first_test()
+		{
+			
+			System.out.println("Hello world");
+			
+			
+			
+		}
+
+	}
+
+
+
